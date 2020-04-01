643e7ac85806b5907ce0c3badd5c88dc7eecae3e
Sahiba Sachdeva <52813693+sahibasachdeva@users.noreply.github.com>
2020-04-01 13:44:47 -0400

    Update Constants.cs

--- Model/Constants.cs
+++ Model/Constants.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -14,7 +14,20 @@ namespace Assignment_4.Model
             public const string BaseUrl = "ftp://waws-prod-dm1-127.ftp.azurewebsites.windows.net/bdat1001-20914";
 
             public const int OperationPauseTime = 10000;
-            public static string remoteUploadFileDestination = "/200449112 Sahiba Sachdeva/students.";
+            public static string remoteUploadFileDestination = "/200449112 Sahiba Sachdeva/info.";
+        }
+
+        public class Locations
+        {
+            public readonly static string ExePath = Environment.CurrentDirectory;
+            public readonly static string ContentFolder = $"{ExePath}\\..\\..\\..\\Content";
+            public readonly static string StudentFolder = $"{ContentFolder}\\StudentFolder";
+            public readonly static string StudentDataFolder = $"{StudentFolder}\\Data";
+            public readonly static string BaseUrl = "https://jsonplaceholder.typicode.com/users";
+            public readonly static string DocLoc = $@"D:\BDAT\InformationEncoding\info.docx";
+            public readonly static string ExcelLoc = $@"D:\BDAT\InformationEncoding\info.xlsx";
+            public readonly static string PptLoc = $@"D:\BDAT\InformationEncoding\info.pptx";
+
         }
     }
 }
